4eb43d55f0ec201fd27a2151eb8b97c511de0451
akash2061 <lucifermorningstar2061@gmail.com>
2024-10-21 00:22:05 +0530

    added rodio for bell

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,23 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use rodio::{Decoder, OutputStream, Sink};
+use std::io::Cursor;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn play_bell_sound() {
+    let (_stream, handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&handle).unwrap();
+
+    let bell_sound = include_bytes!("../assets/bell.wav");
+    let cursor = Cursor::new(bell_sound.as_ref());
+    let source = Decoder::new(cursor).unwrap();
+
+    sink.append(source);
+    sink.sleep_until_end();
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![play_bell_sound])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
