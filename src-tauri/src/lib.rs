4311f6dfe3f5f10812f22c4bebe495964e764f9e
akash2061 <lucifermorningstar2061@gmail.com>
2024-10-30 13:45:53 +0530

    Comment new window

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,10 @@
 use rodio::{Decoder, OutputStream, Sink};
 use std::io::Cursor;
-use tauri::{AppHandle, Builder, Manager, WindowBuilder};
+use tauri::{AppHandle, Builder};
+// use tauri::{LogicalPosition, LogicalSize, Manager, WebviewUrl, WindowBuilder};
 
 #[tauri::command]
-async fn play_bell_sound(app_handle: AppHandle) {
+async fn play_bell_sound(_app_handle: AppHandle) {
     let (_stream, handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&handle).unwrap();
 
@@ -12,29 +13,42 @@ async fn play_bell_sound(app_handle: AppHandle) {
     let source = Decoder::new(cursor).unwrap();
 
     sink.append(source);
-    create_notification_window(app_handle);
+    // let _ = create_notification_window(app_handle);
     sink.sleep_until_end();
 }
 
-#[tauri::command]
-fn create_notification_window(app_handle: AppHandle) {
-    let screen_size = app_handle.get_window("main").unwrap().inner_size().unwrap();
-    let window_width = 300.0; // Your window width
-    let window_height = 150.0; // Your window height
+// #[tauri::command]
+// fn create_notification_window(app_handle: AppHandle) -> Result<(), tauri::Error> {
+//     // let window = app_handle.get_window("main").unwrap().inner_size().unwrap();
+//     // let window_width = 300.0; // Your window width
+//     // let window_height = 150.0; // Your window height
+//     let width = 800.;
+//     let height = 600.;
+//     let window = tauri::window::WindowBuilder::new(&app_handle, "main2")
+//         .inner_size(width, height)
+//         .build()?;
 
-    // Calculate the position for the center-top placement
-    let x_position = (screen_size.width as f64) * 1.35;
-    let y_position = 0.0; // Top of the screen
+//     let _webview1 = window.add_child(
+//         tauri::webview::WebviewBuilder::new("main1", WebviewUrl::App("index1.html".into()))
+//             .auto_resize(),
+//         LogicalPosition::new(0., 0.),
+//         LogicalSize::new(width / 2., height / 2.),
+//     )?;
+//         Ok(())
 
-    let _notification_window = WindowBuilder::new(&app_handle, "notification")
-        .title("Notification")
-        .inner_size(window_width, window_height)
-        .position(x_position, y_position)
-        .always_on_top(true)
-        .resizable(false)
-        .build()
-        .expect("Failed to create notification window");
-}
+//     // // Calculate the position for the center-top placement
+//     // let x_position = (window.width as f64) * 1.35;
+//     // let y_position = 0.0; // Top of the screen
+
+//     // let _notification_window = WindowBuilder::new(&app_handle, "notification")
+//     //     .title("Notification")
+//     //     .inner_size(window_width, window_height)
+//     //     .position(x_position, y_position)
+//     //     .always_on_top(true)
+//     //     .resizable(false)
+//     //     .build()
+//     //     .expect("Failed to create notification window");
+// }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -42,7 +56,7 @@ pub fn run() {
         .plugin(tauri_plugin_notification::init())
         .invoke_handler(tauri::generate_handler![
             play_bell_sound,
-            create_notification_window
+            // create_notification_window
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
